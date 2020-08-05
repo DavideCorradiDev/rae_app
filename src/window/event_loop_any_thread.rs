66bd98444e2b94da8d5b375cd8c358415b3c5e0e
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-05 21:23:01 +0200

    Defined winit re-exports

---
+++ src/window/event_loop_any_thread.rs
@@ -0,0 +1,26 @@
+extern crate winit;
+
+#[cfg(target_os = "windows")]
+pub use winit::platform::windows::EventLoopExtWindows as EventLoopExt;
+
+#[cfg(target_os = "linux")]
+pub use winit::platform::unix::EventLoopExtUnix as EventLoopExt;
+
+pub trait EventLoopAnyThread<T: 'static> {
+    fn new_any_thread() -> winit::event_loop::EventLoop<T>;
+}
+
+impl<T> EventLoopAnyThread<T> for winit::event_loop::EventLoop<T>
+where
+    T: 'static,
+{
+    #[cfg(any(target_os = "linux", target_os = "windows"))]
+    fn new_any_thread() -> winit::event_loop::EventLoop<T> {
+        <winit::event_loop::EventLoop<T> as EventLoopExt>::new_any_thread()
+    }
+
+    #[cfg(not(any(target_os = "linux", target_os = "windows")))]
+    fn new_any_thread() -> winit::event_loop::EventLoop<T> {
+        winit::event_loop::EventLoop::<T>::with_user_event()
+    }
+}
