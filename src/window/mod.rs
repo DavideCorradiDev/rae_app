93519a72c92e48566427850b7d75c66410c302cc
Davide Corradi <davide.corradi.dev@gmail.com>
2020-08-05 20:44:03 +0200

    Winit re-export

---
+++ src/window/mod.rs
@@ -0,0 +1,16 @@
+extern crate winit;
+
+pub use winit::{
+    dpi::*,
+    error::*,
+    event::{Event, *},
+    event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget},
+    window::*,
+};
+
+pub mod keyboard {
+    pub use winit::event::{ModifiersState, ScanCode, VirtualKeyCode as KeyCode};
+}
+
+pub mod event_loop_any_thread;
+pub use event_loop_any_thread::EventLoopAnyThread;
